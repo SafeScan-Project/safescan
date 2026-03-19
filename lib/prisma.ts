e32df781c24e5d703af0a021020c0065693787a9
Sascha <saschaed01@gmail.com>
2026-03-19 13:35:16 -0400

    chore: update safescan changes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,15 +3,7 @@ import { PrismaClient } from '@prisma/client';
 // Prevent multiple Prisma instances in dev
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-const datasourceUrl = process.env.DATABASE_URL || 'file:./prisma/dev.db';
-
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
-    datasources: {
-      db: { url: datasourceUrl },
-    },
-  });
+  globalForPrisma.prisma || new PrismaClient({ log: ['query'] });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
